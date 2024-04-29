f8973b83c87d0897d6bb24612e380b010672b7f5
Alejo Flores Lucey <alejo@misterflores.com>
2024-04-29 11:32:16 -0300

    Add ECM calculation
    Co-authored-by: Nehuen Llanos <nllanos@itba.edu.ar>

--- animation/oscilator.py
+++ animation/oscilator.py
@@ -18,16 +18,29 @@ def get_analytic_solution(times):
     return positions
 
 
+def calculate_ecm(real, approx):
+    differences = []
+    for r, a in zip(real, approx):
+        differences.append((r - a)**2)
+    return np.mean(differences)
+
+
 with (open(os.path.join(os.path.dirname(__file__), "..", "verlet.txt")) as verlet_file,
       open(os.path.join(os.path.dirname(__file__), "..", "beeman.txt")) as beeman_file,
       open(os.path.join(os.path.dirname(__file__), "..", "gear_predictor.txt")) as gear_predictor_file):
     verlet = list(csv.reader(verlet_file, delimiter=" "))
     beeman = list(csv.reader(beeman_file, delimiter=" "))
     gear_predictor = list(csv.reader(gear_predictor_file, delimiter=" "))
+
     time = [float(x[0]) for x in verlet]
+    position_real = get_analytic_solution(time)
     position_verlet = [float(x[1]) for x in verlet]
     position_beeman = [float(x[1]) for x in beeman]
     position_gear_predictor = [float(x[1]) for x in gear_predictor]
+
+    print("ECM Verlet: ", calculate_ecm(position_real, position_verlet))
+    print("ECM Beeman: ", calculate_ecm(position_real, position_beeman))
+    print("ECM Gear Predictor: ", calculate_ecm(position_real, position_gear_predictor))
 
     plt.rcParams.update({'font.size': 20})
     fig, ax = plt.subplots()
@@ -35,7 +48,7 @@ with (open(os.path.join(os.path.dirname(__file__), "..", "verlet.txt")) as verle
     lines.append(ax.plot(time, position_verlet, label="Verlet")[0])
     lines.append(ax.plot(time, position_beeman, label="Beeman")[0])
     lines.append(ax.plot(time, position_gear_predictor, label="Gear Predictor")[0])
-    lines.append(ax.plot(time, get_analytic_solution(time), label="Real", linestyle="dotted")[0])
+    lines.append(ax.plot(time, position_real, label="Real", linestyle="dotted")[0])
 
     ax.set_xlabel("Tiempo $(s)$", fontdict={"weight": "bold"})
     ax.set_ylabel("Posición $(m)$", fontdict={"weight": "bold"})
