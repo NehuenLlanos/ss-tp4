f8f70270d43dc2040427459a9c9fcb38812c027f
Alejo Flores Lucey <alejo@misterflores.com>
2024-05-04 21:22:04 -0300

    Add textures to animation objects

--- animation/mission_animation.py
+++ animation/mission_animation.py
@@ -2,6 +2,7 @@ import csv
 import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptchs
+from PIL import Image
 from matplotlib.animation import FuncAnimation, FFMpegWriter
 
 ########### CONSTANT VARIABLES ###########
@@ -9,7 +10,8 @@ FILENAME = "mars_mission"
 SUN_RADIUS = 6.96340 * 10**7
 EARTH_RADIUS = 6.371 * 10**6.5
 MARS_RADIUS = 3.3895 * 10**6.5
-SPACESHIP_RADIUS = 1000 * 10**3.5
+SPACESHIP_RADIUS = 2000 * 10**4
+AXIS_LIMIT = 3 * 10**8
 ##########################################
 
 
@@ -20,20 +22,45 @@ with open(os.path.join(os.path.dirname(__file__), "..", f"{FILENAME}.txt")) as d
             data[i][j] = float(data[i][j])
 
     fig, ax = plt.subplots()
+    sun_texture = Image.open(os.path.join(os.path.dirname(__file__), "textures", "sun.jpeg"))
+    earth_texture = Image.open(os.path.join(os.path.dirname(__file__), "textures", "earth.jpeg"))
+    mars_texture = Image.open(os.path.join(os.path.dirname(__file__), "textures", "mars.jpeg"))
+    spaceship_texture = Image.open(os.path.join(os.path.dirname(__file__), "textures", "spaceship.png"))
 
     def update(i):
         ax.clear()
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
-        ax.set_xlim(-3 * 10**8, 3 * 10**8)
-        ax.set_ylim(-3 * 10**8, 3 * 10**8)
+        ax.set_xlim(-AXIS_LIMIT, AXIS_LIMIT)
+        ax.set_ylim(-AXIS_LIMIT, AXIS_LIMIT)
         ax.set_aspect('equal', adjustable='box')
 
         # Plot each object
-        ax.add_patch(ptchs.Circle((0, 0), SUN_RADIUS, color='y'))
-        ax.add_patch(ptchs.Circle((float(data[i][1]), float(data[i][2])), EARTH_RADIUS, color='b'))
-        ax.add_patch(ptchs.Circle((float(data[i][3]), float(data[i][4])), MARS_RADIUS, color='r'))
-        ax.add_patch(ptchs.Circle((float(data[i][5]), float(data[i][6])), SPACESHIP_RADIUS, color='k'))
+        spaceship_patch = ptchs.Circle((data[i][5], data[i][6]), SPACESHIP_RADIUS, transform=ax.transData)
+        sun_patch = ptchs.Circle((0, 0), SUN_RADIUS, transform=ax.transData)
+        earth_patch = ptchs.Circle((data[i][1], data[i][2]), EARTH_RADIUS, transform=ax.transData)
+        mars_patch = ptchs.Circle((data[i][3], data[i][4]), MARS_RADIUS, transform=ax.transData)
+
+        ax.imshow(
+            spaceship_texture,
+            extent=(data[i][5] - SPACESHIP_RADIUS, data[i][5] + SPACESHIP_RADIUS,
+                    data[i][6] - SPACESHIP_RADIUS, data[i][6] + SPACESHIP_RADIUS)
+        ).set_clip_path(spaceship_patch)
+        ax.imshow(
+            sun_texture,
+            extent=(-SUN_RADIUS, SUN_RADIUS,
+                    -SUN_RADIUS, SUN_RADIUS)
+        ).set_clip_path(sun_patch)
+        ax.imshow(
+            earth_texture,
+            extent=(data[i][1] - EARTH_RADIUS, data[i][1] + EARTH_RADIUS,
+                    data[i][2] - EARTH_RADIUS, data[i][2] + EARTH_RADIUS)
+        ).set_clip_path(earth_patch)
+        ax.imshow(
+            mars_texture,
+            extent=(data[i][3] - MARS_RADIUS, data[i][3] + MARS_RADIUS,
+                    data[i][4] - MARS_RADIUS, data[i][4] + MARS_RADIUS)
+        ).set_clip_path(mars_patch)
 
         return ax
 
@@ -41,6 +68,6 @@ with open(os.path.join(os.path.dirname(__file__), "..", f"{FILENAME}.txt")) as d
     ani = FuncAnimation(fig, update, frames=len(data), blit=False, interval=1)
 
     # Display the animation
-    plt.show()
+    # plt.show()
     # Save the animation
-    # ani.save("../animation.mp4", writer=FFMpegWriter(fps=30))
+    ani.save("../animation.mp4", writer=FFMpegWriter(fps=30))
